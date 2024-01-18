128d8e1b765062e1e9a81e6251a9f16c35273c4f
Conor O'Brien <Conor.O'Brien@astronics.com>
2024-01-18 15:58:07 -0800

    moving to sidePanel

--- PortalExt/background.js
+++ PortalExt/background.js
@@ -1,5 +1,34 @@
-let color = '#3aa757';
+const PORTAL_ORIGIN = 'http://apps.custom-control.com';
+
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({color});
-    console.log('Default background color set to %cgreen', `color:${color}`);
-})
+    chrome.contextMenus.create({
+        id: 'openSidePanel',
+        title: 'Open side panel',
+        contexts: ['all']
+    });
+});
+
+chrome.sidePanel
+    .setPanelBehavior({ openPanelOnActionClick: true })
+    .catch((error) => console.error(error));
+
+chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
+    if (!tab.url) return;
+    checkURL(tab, tabId);
+});
+
+const checkURL = async (tab, tabId) => {
+    const url = new URL(tab.url);
+    if(url.origin.includes(PORTAL_ORIGIN)) {
+        await chrome.sidePanel.setOptions({
+            tabId,
+            path: 'sidepanel.html',
+            enabled: true
+        });
+    } else {
+        await chrome.sidePanel.setOptions({
+            tabId,
+            enabled: false
+        });
+    }
+}
