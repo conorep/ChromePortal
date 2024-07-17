8b88d424347e4471f083839769340a9702ac2868
Conor O'Brien <Conor.O'Brien@astronics.com>
2024-07-16 17:41:36 -0700

    side panel script now has function to send a message with button clicks

--- PortalExt/html/sidepanelScripts/sidePanelUtilities.js
+++ PortalExt/html/sidepanelScripts/sidePanelUtilities.js
@@ -1,1 +1,10 @@
-chrome.runtime.connect({name: 'sidePanelUtil'});
+chrome.runtime.connect({ name: 'sidePanelUtil' });
+
+document.addEventListener('click', (e) => {
+  let currBtn = e.target.id;
+  chrome.runtime.sendMessage({message: 'tryInsert', btnID: currBtn}, function(response) {
+    if(chrome.runtime.lastError) {
+      return true;
+    }
+  });
+})
