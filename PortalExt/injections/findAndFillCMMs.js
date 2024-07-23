906d61eedd06e2dd88fda7f0e35723b08fcc2e65
Conor O'Brien <Conor.O'Brien@astronics.com>
2024-07-23 14:42:22 -0700

    improved the CMM-filler functionality

--- PortalExt/injections/findAndFillCMMs.js
+++ PortalExt/injections/findAndFillCMMs.js
@@ -1,31 +1,20 @@
 /**
- * This content script, when injected by the extension's service worker, does one of two things:
- * 1. finds a unit's part number, removes all but the prefix, and saves that to storage
- * 2. finds the CMM input, gets the CMM storage value, and injects it (with the -CMM-01 text) into the input,
- *  then removes the value from storage
+ * This content script, when injected by the extension's service worker: finds the CMM input, looks at the parent
+ *  window to get + parse data used in building the input string, then inserts that string.
  */
 (() => {
   const partNumID = 'ctl00_ctl00_cphSite_cphReturn_lblPart';
   const CMMInputID = 'txtDrawing';
-  const cmmText = 'currCMMText';
-
-  let partNumVar = document.getElementById(partNumID);
-  if(partNumVar) {
-    let partVal = partNumVar.innerText;
-
-    if(partVal && partVal !== '') {
-      partVal = partVal.split('-')[0];
-      chrome.storage.local.set({currCMMText: partVal})
-    }
-  }
 
   let cmmInVar = document.getElementById(CMMInputID)
   if(cmmInVar) {
-    chrome.storage.local.get(cmmText).then((res) => {
-      if(res?.currCMMText) {
-        cmmInVar.value = res.currCMMText + '-CMM-01';
-        chrome.storage.local.remove(cmmText);
+    let partNumVar = window.parent.document.getElementById(partNumID);
+    if(partNumVar) {
+      let partVal = partNumVar.innerText;
+      if(partVal && partVal !== '') {
+        partVal = partVal.split('-')[0];
+        cmmInVar.value = partVal + '-CMM-01';
       }
-    })
+    }
   }
 })();
