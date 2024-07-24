d883b14ec3e5677978ec5ba31544113015d9d925
Conor O'Brien <Conor.O'Brien@astronics.com>
2024-07-24 16:35:36 -0700

    simplified code and removed double quotes

--- PortalExt/injections/fillVerbiage.js
+++ PortalExt/injections/fillVerbiage.js
@@ -4,39 +4,33 @@
  * @param btnName string button ID
  */
 function fillVerbiage(btnName) {
-  if(btnName === 'singlePassed' || btnName === 'multiPassed')
-    addPassed(btnName);
-  else if(btnName === 'singleBatt' || btnName === 'multiBatt')
-    batteryWork(btnName);
-  else if(btnName === 'moddedBig')
-    modded();
+  let iFrame = document.getElementById('dlgFrame');
+  if(iFrame != null) {
+    if(btnName === 'singlePassed' || btnName === 'multiPassed')
+      addPassed(btnName);
+    else if(btnName === 'singleBatt' || btnName === 'multiBatt')
+      batteryWork(btnName);
+    else if(btnName === 'moddedBig')
+      modded();
+  }
 
   function addPassed(plurality) {
-    let iFrame = document.getElementById("dlgFrame");
-    if(iFrame != null) {
-      let howMany = plurality === 'singlePassed' ? '' : 's';
-      howMany = " After reassembly, the unit" + howMany + " passed full functional testing."
-      dataInsert(iFrame, howMany)
-    }
+    let howMany = plurality === 'singlePassed' ? '' : 's';
+    howMany = ' After reassembly, the unit' + howMany + ' passed full functional testing.'
+    dataInsert(iFrame, howMany)
   }
 
   function batteryWork(plurality) {
-    let iFrame = document.getElementById("dlgFrame");
-    if(iFrame != null) {
-      let howMany = plurality === 'singleBatt' ? ['each unit\'s depleted batteries', 's'] :
-        ['the unit\'s depleted battery', ''];
-      howMany = "I replaced "+howMany[0]+". After reassembly, the unit"+ howMany[1] +
-        " passed full functional testing."
-      dataInsert(iFrame, howMany);
-    }
+    let howMany = plurality === 'singleBatt' ? ['each unit\'s depleted batteries', 's'] :
+      ['the unit\'s depleted battery', ''];
+    howMany = 'I replaced '+howMany[0]+'. After reassembly, the unit'+ howMany[1] +
+      ' passed full functional testing.'
+    dataInsert(iFrame, howMany);
   }
 
   function modded() {
     const modLang = '\n\nNOTE: This minor modification does not change fit, form, or function of the unit(s).'
-    let iFrame = document.getElementById("dlgFrame");
-    if(iFrame != null) {
-      dataInsert(iFrame, modLang);
-    }
+    dataInsert(iFrame, modLang);
   }
 
   function dataInsert(iFrame, textToInsert) {
