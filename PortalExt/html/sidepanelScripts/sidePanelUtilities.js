8d78f84f9f4abc0db8d1851a1c439c8b555f2561
Conor O'Brien <Conor.O'Brien@astronics.com>
2024-07-24 17:11:55 -0700

    refactoring and adding some functionality to only show one collapse text at a time

--- PortalExt/html/sidepanelScripts/sidePanelUtilities.js
+++ PortalExt/html/sidepanelScripts/sidePanelUtilities.js
@@ -1,22 +1,44 @@
-const alertDivID = 'alertDivPortal';
-const cmmStateID = 'switchBoxCMM';
+const alertDivID = 'alertDivPortal',
+  cmmStateID = 'switchBoxCMM',
+  op10Btn = 'op10Btn',
+  sEnterBtn = 'searchEnterBtn',
+  op10Text = 'op10Insert',
+  searchText = 'searchEnter'
+const flaggedBtnIDs = ['', 'check-circle-fill', op10Btn, sEnterBtn]
 
 window.onload = () => {
   chrome.storage.local.get('cmmState').then((res) => {
     document.getElementById(cmmStateID).checked = res.cmmState;
   })
+
+  const textCollapseArr = [document.getElementById(op10Text), document.getElementById(searchText)];
+  textCollapseArr.forEach((tEle, i) => {
+    tEle.addEventListener('show.bs.collapse', (e) => {
+      let indexLoc = i === 1 ? 0 : 1;
+      if(textCollapseArr[indexLoc].className.includes('show')) {
+        let collapseEle = bootstrap.Collapse.getOrCreateInstance(textCollapseArr[indexLoc]);
+        collapseEle.hide();
+      }
+    })
+    tEle.addEventListener('shown.bs.collapse', () => {
+      tEle.scrollIntoView();
+    })
+  })
 }
 
 document.addEventListener('click', (e) => {
   let currBtn = e.target.id;
-  if(!currBtn || currBtn === '' || currBtn === 'check-circle-fill') {
+  if(!currBtn || currBtn === '' || flaggedBtnIDs.includes(currBtn)) {
     return;
   }
 
   if(currBtn === cmmStateID) {
     chrome.storage.local.set({ cmmState: e.target.checked });
     chrome.runtime.sendMessage({ cmmState: e.target.checked }, (response) => {
-      console.log(response);
+      const inMsg = 'CMM text input function change successful!';
+      if(!document.getElementById(alertDivID)) {
+        insertTheAlert(inMsg)
+      }
     })
     return;
   }
@@ -26,7 +48,6 @@ document.addEventListener('click', (e) => {
       console.log(chrome.runtime.lastError);
       return true;
     }
-    console.log(response);
     if(response.insert === 'good') {
       const inMsg = 'Text insert successful!';
       if(!document.getElementById(alertDivID)) {
