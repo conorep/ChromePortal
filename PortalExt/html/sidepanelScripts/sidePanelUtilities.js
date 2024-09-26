b26b492d4d40adca90b731b3ebb341da6e136ae9
Conor O'Brien <Conor.O'Brien@astronics.com>
2024-09-25 17:16:04 -0700

    migrated to arrow functions, moved arrow funcs to top of script, and added 'checkeditingpage'

--- PortalExt/html/sidepanelScripts/sidePanelUtilities.js
+++ PortalExt/html/sidepanelScripts/sidePanelUtilities.js
@@ -6,14 +6,52 @@ const alertDivID = 'alertDivPortal',
   searchText = 'searchEnter'
 const flaggedBtnIDs = ['', 'check-circle-fill', op10Btn, sEnterBtn]
 
+const triggerSuccessAlert = (alertDiv) => {
+  let alertInstance = bootstrap.Alert.getOrCreateInstance(alertDiv);
+
+  setTimeout(() => {
+    alertInstance.close();
+  }, 2000);
+}
+
+const insertTheAlert = (alertMsg) => {
+  let createdEle = document.createElement('div');
+  createdEle.classList.add('alert-floater');
+  createdEle.innerHTML = `
+<div class='alert alert-success d-flex align-items-center alert-dismissible fade show' role='alert' id='alertDivPortal'>
+  <svg class='bi flex-shrink-0 me-2' width='24' height='24' role='img' aria-label='Success:'>
+  <use xlink:href="#check-circle-fill"/></svg>
+  <div id='alertMsgPortal'>${alertMsg}</div>
+</div>
+`;
+  document.body.append(createdEle);
+
+  let alertDivEle = document.getElementById(alertDivID);
+  triggerSuccessAlert(alertDivEle);
+}
+
+const checkAlert = (msg) => {
+  if(!document.getElementById(alertDivID)) {
+    insertTheAlert(msg)
+  }
+}
+
+const checkPageEditing = () => {
+  const queryP = window.location.search;
+  console.log(queryP);
+  if(queryP?.split?.('=')?.[1] === 'true') {
+    document.getElementById('certStuff').classList.toggle('slideIntoView');
+  }
+}
+
 window.onload = () => {
   chrome.storage.local.get('cmmState').then((res) => {
     document.getElementById(cmmStateID).checked = res.cmmState;
   })
-
+  checkPageEditing();
   const textCollapseArr = [document.getElementById(op10Text), document.getElementById(searchText)];
   textCollapseArr.forEach((tEle, i) => {
-    tEle.addEventListener('show.bs.collapse', (e) => {
+    tEle.addEventListener('show.bs.collapse', () => {
       let indexLoc = i === 1 ? 0 : 1;
       if(textCollapseArr[indexLoc].className.includes('show')) {
         let collapseEle = bootstrap.Collapse.getOrCreateInstance(textCollapseArr[indexLoc]);
@@ -51,33 +89,3 @@ document.addEventListener('click', (e) => {
     }
   });
 })
-
-function checkAlert(msg) {
-  if(!document.getElementById(alertDivID)) {
-    insertTheAlert(msg)
-  }
-}
-
-function insertTheAlert(alertMsg) {
-  let createdEle = document.createElement('div');
-  createdEle.classList.add('alert-floater');
-  createdEle.innerHTML = `
-<div class='alert alert-success d-flex align-items-center alert-dismissible fade show' role='alert' id='alertDivPortal'>
-  <svg class='bi flex-shrink-0 me-2' width='24' height='24' role='img' aria-label='Success:'>
-  <use xlink:href="#check-circle-fill"/></svg>
-  <div id='alertMsgPortal'>${alertMsg}</div>
-</div>
-`;
-  document.body.append(createdEle);
-
-  let alertDivEle = document.getElementById(alertDivID);
-  triggerSuccessAlert(alertDivEle);
-}
-
-function triggerSuccessAlert(alertDiv) {
-  let alertInstance = bootstrap.Alert.getOrCreateInstance(alertDiv);
-
-  setTimeout(() => {
-    alertInstance.close();
-  }, 2000);
-}
