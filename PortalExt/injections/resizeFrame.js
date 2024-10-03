ce4316bf25ec8f938a30dbcca73e2627fb8ee520
Conor O'Brien <Conor.O'Brien@astronics.com>
2024-10-02 17:16:33 -0700

    added a script that fixes the main UI issue with the 'Operations' modal in portal

---
+++ PortalExt/injections/resizeFrame.js
@@ -0,0 +1,29 @@
+/**
+ * Make the "Operation" modal's resizing fit the changes to the textarea element in the modal.
+ */
+if(window === window.top) {
+  const INIT_WIDTH = 750;
+  let dlgFrame = document.getElementById('dlgFrame');
+
+  if(dlgFrame) {
+    dlgFrame.style.height = '100%';
+    let frameDoc = dlgFrame.contentWindow.document,
+      frameBody = frameDoc.getElementsByTagName('BODY')?.[0],
+      frameTextArea = frameDoc.getElementById('txtNotes1');
+
+    if(frameTextArea) {
+      let dialogDiv = document.getElementById('divDialog');
+
+      let moveListener = () => {
+        dialogDiv.style.height = frameBody.offsetHeight + 40 + 'px';
+        let textWidth = INIT_WIDTH > frameTextArea.offsetWidth ? INIT_WIDTH : frameTextArea.offsetWidth;
+        dialogDiv.style.width = textWidth + 20 + 'px';
+      }
+
+      frameTextArea.addEventListener('mousedown', () => {
+        frameDoc.addEventListener('mousemove', moveListener)
+        frameDoc.addEventListener('mouseup', moveListener)
+      })
+    }
+  }
+}
