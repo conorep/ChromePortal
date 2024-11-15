27fda4e3c0bdd0357c693ea4f01ea58adc88d59e
Conor O'Brien <Conor.O'Brien@astronics.com>
2024-11-15 14:06:41 -0800

    added two more annoying alert text variables to check for and simplified from 'if' to an every loop

--- PortalExt/injections/alertHandle.js
+++ PortalExt/injections/alertHandle.js
@@ -1,10 +1,20 @@
 window.originalAlert = window.alert;
 window.alert = function() {
-  if(arguments[0].startsWith('Warning!  If you change any of the following info')) {
-    console.log('skip this alert!', arguments);
+  let alertArray = ['Warning!  If you change any of the following info', 'To complete the', 'To generate the'];
+
+  let foundAlert = false;
+  alertArray.every((alertText) => {
+    if(arguments[0].startsWith(alertText)) {
+      console.log('skip this alert!', arguments);
+      foundAlert = true;
+      return false;
+    }
     return true;
+  })
+
+  if(!foundAlert) {
+    return window.originalAlert(arguments[0]);
   }
-  return window.originalAlert(arguments[0]);
 }
 window.originalConfirm = window.confirm;
 window.confirm = function() {
