efec45baadfbff14a682f03eac0e9f92b5fe834e
Conor O'Brien <Conor.O'Brien@astronics.com>
2025-05-01 17:26:12 -0700

    new script that programmatically creates CMM text input

---
+++ PortalExt/injections/setCMM.js
@@ -0,0 +1,20 @@
+/**
+ *  This script tunes up a pre-existing automated manual name creator.
+ */
+
+function loseFocusHandler(e) {
+  let currTextVal = e.target.value;
+  if(currTextVal.trim() !== '' && currTextVal.length > 4) {
+    const mainManualInput = document.getElementById('txtCompMaintManual');
+    //NEXT UP: add some invalidators tied to purchased parts that should have no CMM
+    if(mainManualInput && mainManualInput.value === 'No Valid CMM' || mainManualInput.value.trim() === '') {
+      currTextVal = currTextVal.slice(0, 3).toUpperCase() + '-CMM-01';
+      mainManualInput.value = currTextVal;
+    }
+  }
+}
+
+if(window === window.top && window.location.pathname.endsWith('editReturn.aspx')) {
+  const partNumInput = document.getElementById('txtPart');
+  if(partNumInput) partNumInput.addEventListener('blur', loseFocusHandler);
+}
