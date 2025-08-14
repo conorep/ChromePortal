c1313b46a42941bd829b1032c596d0781a85aad6
Conor O'Brien <Conor.O'Brien@astronics.com>
2025-08-14 11:27:27 -0700

    refactor * function --> arrow function * slight change to file jsDoc

--- PortalExt/injections/setMinPageWidth.js
+++ PortalExt/injections/setMinPageWidth.js
@@ -1,10 +1,8 @@
 /**
- * This script sets a minimum size for certain elements to disallow layout disarray.
+ * This content script sets a minimum size for certain elements to disallow layout disarray.
  */
 if(window === window.top) {
-  function minWidth950(element) {
-    element.style.minWidth = '950px';
-  }
+  const minWidth950 = (element) => element.style.minWidth = '950px';
   const elementArr = ['body', '#appHeader'];
   elementArr.every((eleName) => {
     minWidth950(document.querySelector(eleName));
