1332d0c4e7d5d57ce6fafef2b89703fd4faadb76
Conor O'Brien <Conor.O'Brien@astronics.com>
2025-08-14 11:40:39 -0700

    refactoring and enhancing functions * triggering change event after value alteration * using string val formatted as float * reducing redundant code

--- PortalExt/injections/zeroQuoteAdminHours.js
+++ PortalExt/injections/zeroQuoteAdminHours.js
@@ -2,12 +2,19 @@
  * This content script finds the service quote 'Administrative Processing' time inputs and changes them to 0 hours.
  */
 const zeroAdminTime = () => {
-  const adminTimeNonWarranty = document.getElementById('txtAdminTimeNW');
-  const adminTimeWarranty = document.getElementById('txtAdminTimeW');
-  if(adminTimeNonWarranty)
-    adminTimeNonWarranty.value = 0;
-  if(adminTimeWarranty)
-    adminTimeWarranty.value = 0;
+  const adminTimeNonWarranty = document.getElementById('txtAdminTimeNW'),
+    adminTimeWarranty = document.getElementById('txtAdminTimeW');
+
+  if(!adminTimeNonWarranty || !adminTimeWarranty) return;
+
+  const changeEvent = new Event('change', { bubbles: true });
+  const changeValue = (el) => {
+    el.value = '0.00';
+    el.dispatchEvent(changeEvent);
+  }
+
+  changeValue(adminTimeNonWarranty);
+  changeValue(adminTimeWarranty);
 }
 
 window === window.top && window.location.pathname.endsWith('editReturn.aspx') && zeroAdminTime();
