72ad55086429b0c6c2be90aaf4a287b3470b999e
Conor O'Brien <Conor.O'Brien@astronics.com>
2025-08-15 19:04:49 -0700

    styling and altering page to move and show elements appropriately

--- PortalExt/injections/fixInfoElements.js
+++ PortalExt/injections/fixInfoElements.js
@@ -2,24 +2,59 @@
  * This content script fixes the layout of the info-containing divs in the 'Info' tab of portal.
  */
 (() => {
-  const infoTab = 'tabs-info';
-  const removedStyles = ['width', 'height'];
+  const infoTab = 'tabs-info',
+    removedStyles = ['width', 'height', 'margin-bottom'],
+    newClass = ['certInfo', 'aircraftInfo', 'partDetails', 'returnInfo'],
+    dynamicMargins = [1, 2, 3, 3, 1]; // 1 is left margin, 2 is right, 3 is both
+
+  const isEditing = window.location.pathname.endsWith('editReturn.aspx'),
+    infoVar = document.getElementById(infoTab);
+
+  const keepInfoTabVisible = () => {
+    if(!isEditing) return;
+
+    infoVar.style.minHeight = '410px';
+    infoVar.style.overflowY = 'auto';
+  }
+
   const removeStyleProp = (child) => {
     removedStyles.forEach((aStyle) => {
       child.style.removeProperty(aStyle);
     })
-    if(child.nodeName === 'FIELDSET') {
-      child.style.setProperty('margin-top', '10px');
-    }
   }
 
-  const infoVar = document.getElementById(infoTab);
+  const addOrRemoveClass = (element, cName) => {
+    if(isEditing)
+      element.classList.add(cName);
+    else
+      element.classList.remove(cName);
+  }
+
+  const setMargins = (element, indexLoc) => {
+    if(indexLoc > 4) return;
+
+    let marginNum = isEditing ? '0' : '15px',
+      indexCode = dynamicMargins[indexLoc];
+
+    if(indexCode === 1 || 3)
+      element.style.marginLeft = marginNum;
+
+    if(indexCode === 2 || 3)
+      element.style.marginRight = marginNum;
+  }
+
   if(infoVar && window === window.top) {
+    keepInfoTabVisible();
+    addOrRemoveClass(infoVar, 'infoGrid');
+
     [...infoVar.children].forEach((childDiv, i) => {
       if(i < 4) {
+        addOrRemoveClass(childDiv, newClass[i]);
         removeStyleProp(childDiv);
       }
+      setMargins(childDiv, i);
     })
+
     const bottomBar = document.getElementsByClassName('bottomBar')[1];
     if(bottomBar) {
       [...bottomBar.children].forEach((childAnchor) => {
